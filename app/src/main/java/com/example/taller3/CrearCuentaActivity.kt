4bd14abc1d28689e0b1e37d259383d920920ec0a
ElianaCepeda <eli.cepedagonzalez@gmail.com>
2025-05-12 18:01:14 -0500

    Autenticación completa

--- app/src/main/java/com/example/taller3/CrearCuentaActivity.kt
+++ app/src/main/java/com/example/taller3/CrearCuentaActivity.kt
@@ -5,10 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.taller3.databinding.ActivityCrearCuentaBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
